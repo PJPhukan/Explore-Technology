9188f63f3b8460da516436e2189bfcd5a73d8192
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-05-02 19:45:25 +0530

    Testimonials section completed

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -5,7 +5,7 @@ import { Spotlight } from "@/components/ui/Spotlight";
 
 const HomeSection = () => {
   return (
-    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col justify-center items-center relative mx-auto py-10 md:py-0">
+    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col justify-center items-center relative mx-auto py-10 md:py-0 dark:bg-black">
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
